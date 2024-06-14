f47c7130458b63d958cdc72b3462c46a58fcd003
Ranwiesiel <87268415+Ranwiesiel@users.noreply.github.com>
2024-06-14 22:28:47 +0700

    fix code

---
+++ settings.py
@@ -0,0 +1,64 @@
+import pathlib
+from logging.config import dictConfig
+import logging
+import discord
+import os
+import json
+
+with open("configuration.json", "r") as config: 
+	data = json.load(config)
+	GUILDS_ID = discord.Object(data["guild_id"])
+print(GUILDS_ID)
+BASE_DIR = pathlib.Path(__file__).parent  #path root atau parent
+
+CMDS_DIR = BASE_DIR / "cmds"  #membaut path yang dituju
+COGS_DIR = BASE_DIR / "cogs"
+
+# GUILDS_ID = discord.Object(id=int(os.environ['GUILD']))
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format":
+            "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {
+            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "bot": {
+            "handlers": ['console'],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            "handlers": ['console2', "file"],
+            "level": "INFO",
+            "propagate": False
+        },
+    },
+}
+
+dictConfig(LOGGING_CONFIG)
